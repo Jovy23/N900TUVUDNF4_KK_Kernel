ce00689af7b3e488b93e95633b000afae9162e34
jovy23 <jovyflow3@gmail.com>
2014-08-26 02:46:36 -0400

    Governor: Added Yankactive Governor!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -473,6 +473,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_optimax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
 #endif
 
 
