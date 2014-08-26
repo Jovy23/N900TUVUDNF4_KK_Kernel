c47fc756f5cd769a74b283ce70511aafb4bd9966
jovy23 <jovyflow3@gmail.com>
2014-08-26 02:36:09 -0400

    Governor: Added Optimax Governor!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -470,6 +470,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
+extern struct cpufreq_governor cpufreq_gov_optimax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_optimax)
 #endif
 
 
