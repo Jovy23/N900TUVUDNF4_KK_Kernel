49eb34d13e9d61d940c24576e04f99f339a74999
jovy23 <jovyflow3@gmail.com>
2014-08-28 00:05:37 -0400

    Removed Optimax Governor (Due To Freezes/Random Reboots)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -470,9 +470,6 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
-extern struct cpufreq_governor cpufreq_gov_optimax;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_optimax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
