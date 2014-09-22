976a949abc52d0a8ae58e53486a75592d42c4f94
jovy23 <jovyflow3@gmail.com>
2014-09-22 04:51:41 -0400

    Revert "intelli-thermal v2: initial adaptation"
    This reverts commit c73af823e1809dd35ffaf239b45513ced2128e3b.

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -30,15 +30,9 @@ struct msm_thermal_data {
 	int32_t vdd_rstr_temp_hyst_degC;
 	int32_t psm_temp_degC;
 	int32_t psm_temp_hyst_degC;
-#ifdef CONFIG_INTELLI_THERMAL_V2
-	int32_t ocr_temp_degC;
-	int32_t ocr_temp_hyst_degC;
-#endif
 };
 
-#if defined(CONFIG_THERMAL_MONITOR) ||\
-    defined(CONFIG_INTELLI_THERMAL) ||\
-    defined(CONFIG_INTELLI_THERMAL_V2)
+#if defined(CONFIG_THERMAL_MONITOR) || defined(CONFIG_INTELLI_THERMAL)
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
 #else
